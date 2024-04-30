84c1833cb1d64c576871f798e7d48bf0c411909f
Sofia Tombazzi <128006502+sofitombazzi@users.noreply.github.com>
2024-04-30 13:32:56 -0300

    Pruebo que funcione el fetch

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,10 @@
 //Creo una constante donde guardar la URL de la pagina donde voy a sacar los datos
-const URL_POKEMON = "https://pokeapi.co/api/v2/pokemon/ditto";
+const URL_POKEMON = "https://pokeapi.co/api/v2/pokemon/";
+
+//Pruebo que se vea en consola
+console.log(URL_POKEMON);
+
+//Traigo lo que hay en la URL
+fetch(URL_POKEMON).then(data => {
+    console.log(data);
+});
