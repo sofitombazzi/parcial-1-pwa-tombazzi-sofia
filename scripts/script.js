b5ff69112574a6d2a5bf10be9ad3d1d350c3795d
Sofia Tombazzi <128006502+sofitombazzi@users.noreply.github.com>
2024-04-30 15:33:39 -0300

    feat: Cambie que al hacer click al nombre del pokemon te lleve a la otra pagina Detalles

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 //Creo una constante donde guardar la URL de la pagina donde voy a sacar los datos
-const URL_POKEMON = "https://pokeapi.co/api/v2/pokemon/";
+const URL_POKEMON = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0";
 
 //Pruebo que se vea en consola
 console.log(URL_POKEMON);
@@ -9,12 +9,13 @@ const mostrarTarjeta = (pokemon) => {
     console.log(pokemon);
     //Traigo mi contenedor del HTML
     const contenedor = document.getElementById("contenedor");
-    contenedor.addEventListener('click', () => {
-        mostrarDetalle(pokemon.id);
-    })
+
     //Creo el titulo
     const titulo = document.createElement("h2");
     titulo.innerText = pokemon.name;
+    titulo.addEventListener('click', () => {
+        mostrarDetalle(pokemon.id);
+    })
 
     const descripcion = document.createElement('p');
 
@@ -36,7 +37,7 @@ fetch(URL_POKEMON)
 .then(result => {
     
     console.log(result);
-    const results = result.results;
+    const results = result.results.slice(0,100);
     //Realizo un forEach para recorrer el array y devolver todos los element que es este caso son pokemones
     results.forEach(element => {
         mostrarTarjeta(element);
